fb9414de37d70e767056d1e3c4e2b7c5138a8c54
John <jsok@rheebros.com>
2025-05-13 15:09:18 -0400

    Create a property parentNode to save the path of reached Node.
    Realized that a possible way implement this is through nested iteration. Each outer iteration would be the next breadth/level of knight traversal.
    Within the inner iteration, would be the check of each node within the same level.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function knightMoves(startVertex, endVertex) {
-	let queue = [];
-	let vertices = getPossibleVertices(startVertex);
-	queue = [...queue, ...vertices];
+	const queue = [{position: startVertex, parentNode: null}]
+	const visited = [];
+	while (queue.length != 0) {
+		const node = queue.shift();
+		const adjacentNodes = getPossibleVertices(node.position, queue);
+		adjacentNodes.forEach((element) => {
+
+		})
+	}
 }
 
-function getPossibleVertices(startVertex) {
-	const result = [];
+function getPossibleVertices(startVertex, queue) {
+
 	const [row, col] = startVertex;
 	const rowDifference = [-1, -2, 1, 2];
 	for (const element of rowDifference) {
@@ -21,11 +27,11 @@ function getPossibleVertices(startVertex) {
 			vertexCol2 = col - 1;
 		}
 
-		if (vertexCol1 >= 0 && vertexCol1 <= 7) result.push([vertexRow, vertexCol1]);
-		if (vertexCol2 >= 0 && vertexCol2 <= 7) result.push([vertexRow, vertexCol2]);
+		if (vertexCol1 >= 0 && vertexCol1 <= 7) queue.push({position: [vertexRow, vertexCol1], parentNode: startVertex});
+		if (vertexCol2 >= 0 && vertexCol2 <= 7) queue.push({position: [vertexRow, vertexCol2], parentNode: startVertex});
 	}
 
-	return result;
+	return queue;
 }
 
 console.log(getPossibleVertices([7, 7]));
